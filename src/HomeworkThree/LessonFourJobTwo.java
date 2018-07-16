268c3738f1c49e3851a8266a55df4b3d75190650
Alex <alex3911@mail.ru>
2018-07-16 22:32:33 +0300

    Homework

--- src/HomeworkThree/LessonFourJobTwo.java
+++ src/HomeworkThree/LessonFourJobTwo.java
@@ -6,14 +6,14 @@ public class LessonFourJobTwo {
 		Random rand = new Random();
 	char [] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};		
 				
-    for (int i = 0 ; i <= 3; i++) {
+    for (int i = 0 ; i <= 2; i++) {
     	int  n = rand.nextInt(25);
     	System.out.println ("Введите строчную букву английского алфавита");
 		Scanner d = new Scanner(System.in);
-		char b = (char) d.nextInt();	
+		char b = d.next().charAt(0);	
 
 	if ( a[n] == b) System.out.println ("Вы победили");
-		
+
 	else System.out.println ("Вы проиграли. Попрбуйте еще раз");
 
 	
